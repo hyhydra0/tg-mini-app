90498b47538c4601d5a86cb64de3bee907650ddd
hyhydra0 <hy.hydra@outlook.com>
2024-11-06 02:25:54 +0800

    fix: type error

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-function App() {
+const App: React.FC = () => {
   const [count, setCount] = useState(0);
 
   return (
@@ -29,6 +29,6 @@ function App() {
       </p>
     </>
   );
-}
+};
 
 export default App;
