003e4168e18656851fa501581b75d85c405e0ea0
hyhydra0 <hy.hydra@outlook.com>
2024-11-06 05:40:00 +0800

    fix: back

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,7 @@
 import Layout from "@/layout";
 import event from "@/assets/event.jpg";
-import React from "react";
 
 const App: React.FC = () => {
-  React.useEffect(() => {
-    // Ensure the Telegram WebApp API is available
-    if (window.Telegram.WebApp) {
-      // Initialize the web app
-      window.Telegram.WebApp.ready();
-
-      // Function to handle sending data for different buttons
-      function sendDataForButton(buttonId: string) {
-        window.Telegram.WebApp.sendData(
-          JSON.stringify({ action: "button_click", button_id: buttonId })
-        );
-      }
-
-      // Add event listeners for each button
-      window.document
-        .getElementById("button1")
-        ?.addEventListener("click", function () {
-          sendDataForButton("button1");
-        });
-
-      window.document
-        .getElementById("button2")
-        ?.addEventListener("click", function () {
-          sendDataForButton("button2");
-        });
-
-      window.document
-        .getElementById("button3")
-        ?.addEventListener("click", function () {
-          sendDataForButton("button3");
-        });
-    }
-  });
   return (
     <Layout>
       <div className="flex flex-col p-[16px] gap-[24px]">
@@ -71,12 +37,12 @@ const App: React.FC = () => {
                 </div>
               </div>
             </div>
-            <button
-              id={`button${item}`}
+            <a
+              href="https://projectpolyverse.cpventure.io"
               className="border-[1px] border-solid border-white text-center text-[12px] rounded-[8px] py-[2px]"
             >
               visit
-            </button>
+            </a>
           </div>
         ))}
       </div>
