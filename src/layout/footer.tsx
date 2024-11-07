2ca3b2b0291a32ef7bcbcb3f439810392e3b03cd
hyhydra0 <hy.hydra@outlook.com>
2024-11-07 16:43:03 +0800

    fix: footer and roadmap link

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,7 +1,8 @@
 const Footer = () => {
   return (
     <div className="text-center py-[8px] text-[#B2B2B2]">
-      @copyright {new Date().toDateString().slice(10)} Powered by CP ventures
+      Copyright Polyverse {new Date().toDateString().slice(10)} powered by CP
+      Ventures
     </div>
   );
 };
